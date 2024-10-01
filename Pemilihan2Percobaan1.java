0bf308e90115f65164d62f00e03ade682a9227ca
clovr1 <keenanaryasatya01@gmail.com>
2024-10-01 21:43:59 +0700

    Pemilihan2Percobaan1

--- Pemilihan2Percobaan1.java
+++ Pemilihan2Percobaan1.java
@@ -6,19 +6,25 @@ public class Pemilihan2Percobaan1 {
         Scanner input05 = new Scanner(System.in);
         System.out.print("Masukkan Tahun: ");
         int tahun = input05.nextInt();
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100 != 0) || (tahun % 400 == 0)) {
-                System.out.println("Tahun Kabisat");
+        if (tahun % 4 == 0) {
+            
+            if (tahun % 100 == 0) {
+                
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
             } else {
-                System.out.println("Bukan Tahun Kabisat");
+                
+                System.out.println("Tahun Kabisat");
             }
-        } else if (tahun == 2100) {
-            System.out.println("Bukan Tahun Kabisat");
         } else {
+            
             System.out.println("Bukan Tahun Kabisat");
-            input05.close();
+        }
         }
 
         
     }
-}
+
